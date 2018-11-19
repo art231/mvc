66d1a3cdf6fc85727ddb5134aec742ee529b9db4
art231 <t.artyom.at@gmail.com>
2018-11-19 10:23:34 +0300

    create login route edit

--- SolutionExtJs/SiteExtjs/app.js
+++ SolutionExtJs/SiteExtjs/app.js
@@ -1,48 +1,27 @@
 Ext.application({
     name: 'SiteExtjs',
 
-    // views: [
-        
-    //      //'loginForm'
-    //      //'FieldUserCenterLayout'
-    // ],
-
-    controllers : [
-        //'UserCatalogController',
-        'route'
+    views: [
+        'AddCarFormView',
+        'CarCatalogView',
+        'CarGridView',
+        'SearchCarView'
     ],
 
-    // stores : [
-    //     'CarCatalogStore'
-    // ],
+    controllers : [
+        'CarCatalogController'
+    ],
+
+    stores : [
+        'CarCatalogStore'
+    ],
 
     launch: function () {
-    //     var win = new Ext.Window({
-    //     title: 'Admin Login',             
-    //     layout:'fit',
-    //     width:450,
-    //     height:200,
-    //     //html: '<div id="userAccess"></div>',
-    //     closable: true,
-    //     resizable: false,              
-    //     border: false,
-    //     buttonAlign: 'center',
-    //     items: {
-    //             xtype  : 'loginForm'
-    //         }
-    // });
-    // win.show();
-
-
-    //     Ext.create('Ext.container.Viewport', {
-    //         layout: 'fit',
-    //         //anchor:'100%',
-    //         items: {
-    //             //xtype  : 'FieldUser'
-    //         }
-    //     });
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: {
+                xtype  : 'carCatalogView'
+            }
+        });
     }
 });
-
-//=============================================================================================
-
