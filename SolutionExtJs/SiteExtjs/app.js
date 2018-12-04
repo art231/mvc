d0ea620f7c8640cf370f9b8db8edad59a6f211d2
art231 <t.artyom.at@gmail.com>
2018-12-04 09:35:36 +0300

    correct work

--- SolutionExtJs/SiteExtjs/app.js
+++ SolutionExtJs/SiteExtjs/app.js
@@ -1,12 +1,12 @@
 Ext.application({
     name: 'SiteExtjs',
 
-    // views: [
-    //     //'AddCarFormView',
-    //     'CarCatalogView'
-    //     ////'CarGridView',
-    //     //'SearchCarView'
-    // ],
+    //  views: [
+    // // //     //'AddCarFormView',
+    //       'CarCatalogView'
+    // // //     ////'CarGridView',
+    // // //     //'SearchCarView'
+    //  ],
 
     controllers : [
         'CarCatalogController'
@@ -17,45 +17,7 @@ Ext.application({
     // ],
 
     launch: function () {
-        // Ext.create('Ext.container.Viewport', {
-        //     layout: 'fit',
-        //     items: {
-        //         xtype  : 'carCatalogView'
-        //     }
-        // });
+        
 
-        // var window = Ext.create('Ext.window.Window', {
-        //     title: 'Приложение',
-        //     width: 300,
-        //     height: 200,
-        //     items:[{
-        //             xtype: 'textfield',
-        //             fieldLabel: 'Логин',
-        //             name: 'login',
-        //             labelAlign: 'top',
-        //             //cls: 'field-margin',
-        //             flex: 1
-        //            }, {
-        //             xtype: 'textfield',
-        //             fieldLabel: 'Пароль',
-        //             name: 'password',
-        //             labelAlign: 'top',
-        //             //cls: 'field-margin',
-        //             flex: 1
-        //           }],       
-        // // кнопки формы
-        // buttons: [{
-        //     text: 'Оправить',
-        //     handler: function() {
-        //         // действие отправки
-        //     }
-        // }, {
-        //     text: 'Отмена',
-        //     handler: function() {
-        //         // действие отмены
-        //         }
-        // }]
-        // });
-        // window.show();
     }
 });
--- SolutionExtJs/SiteExtjs/app/controller/CarCatalogController.js
+++ SolutionExtJs/SiteExtjs/app/controller/CarCatalogController.js
@@ -1,3 +1,6 @@
+//ApplicationRouter router=new ApplicationRouter.initRouter();
+
+
 Ext.define('SiteExtjs.controller.CarCatalogController', {
     extend: 'Ext.app.Controller',
 
@@ -14,207 +17,189 @@ Ext.define('SiteExtjs.controller.CarCatalogController', {
 
     onUsers : function() {
         //...
-        console.log("hello");
-	        
-
-	  var formPanel=Ext.create('Ext.form.Panel',{
-        title: 'Форма авторизации',
-        width: 300,
-        height:200,
-        region:'center',
-        //layout: 'anchor',
-        layout: {
-        type: 'vbox',
-        align: 'center',
-        pack: 'center'
-    },
-        //position:'absolute',
-        defaults: {
-            anchor: '80%'
-        },
-        renderTo: 'content',
-        items: [{
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
-
-            // anchor : '95%',
-            // labelWidth : 150
-            //width: '450px'
-            // fieldDefaults: {
-            //     labelWidth: 200
-            // }
-           	//width: '100px'
-            //allowBlank: false
-            //maxLength: 100,
-            //maxWidth: '300px'
-            //labelWidth: 200
-        }, {
-            xtype: 'textfield',
-            name: 'password',
-            inputType: 'password',
-            fieldLabel: 'Password'
-            //width: 300
-            //allowBlank: false
-         }
-         // {
-        //     xtype: 'displayfield',
-        //     hideEmptyLabel: false,
-        //     value: 'Enter any non-blank password'
-        // }
-        ],      
-        // кнопки формы
-        buttons: [{
-            text: 'Оправить',
-            handler: function() {
-                // действие отправки
-            }
-        }, {
-            text: 'Логин',
-            handler: function() {
-                var login = formPanel.items.get(0);
-                alert(login.getValue());
-                }
-        }],
-    });
-
+        console.log(this);
+        
+//     var formPanel=Ext.create('Ext.form.Panel',{
+//         title: 'Форма авторизации',
+//         width: 300,
+//         height:200,
+//         autoEl:'center',
+//         layout: 'anchor',
+//         defaults: {
+//             anchor: '80%'
+//         },
+//         renderTo: 'content',
+//         //autoEl:'center',
+//         items:[{
+//                 xtype: 'textfield',
+//                 fieldLabel: 'Логин',
+//                 name: 'login',
+//                 labelAlign: 'top',
+//                 cls: 'field-margin',
+//                 flex: 1
+//                }, {
+//                 xtype: 'textfield',
+//                 fieldLabel: 'Пароль',
+//                 name: 'password',
+//                 labelAlign: 'top',
+//                 cls: 'field-margin',
+//                 flex: 1
+//               }],       
+//         // кнопки формы
+//         buttons: [{
+//             text: 'Оправить',
+//             handler: function() {
+//                 // действие отправки
+//             }
+//         }, {
+//             text: 'Логин',
+//             handler: function() {
+//                 var login = formPanel.items.get(0);
+//                 alert(login.getValue());
+//                 }
+//         }],
+   
+// });
+	     //var router=   ApplicationRouter.initRouter();
+	    //console.log(router);
+	  
 
     },
     onRegistration : function() {
-    	console.log(this);
-    	Ext.create('Ext.form.Panel', {
-    renderTo: 'content',
-    title: 'User Form',
-    height: 400,
-    width: 300,
-    bodyPadding: 10,
-    layout: {
-        type: 'vbox',
-        align: 'center'
-        //pack: 'center'
-    },
-        //position:'absolute',
-     //fieldLabel:200,
-    defaultType: 'textfield',
-    items: [{
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
+    	//console.log(this);
+//     	Ext.create('Ext.form.Panel', {
+//     renderTo: 'content',
+//     title: 'User Form',
+//     height: 400,
+//     width: 300,
+//     bodyPadding: 10,
+//     layout: {
+//         type: 'vbox',
+//         align: 'center'
+//         //pack: 'center'
+//     },
+//         //position:'absolute',
+//      //fieldLabel:200,
+//     defaultType: 'textfield',
+//     items: [{
+//             xtype: 'textfield',
+//             name: 'username',
+//             fieldLabel: 'Username',
 
-            // anchor : '95%',
-            // labelWidth : 150
-            //width: '450px'
-            // fieldDefaults: {
-            //     labelWidth: 200
-            // }
-           	//width: '100px'
-            //allowBlank: false
-            //maxLength: 100,
-            //maxWidth: '300px'
-            //labelWidth: 200
-        },
-        {
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
+//             // anchor : '95%',
+//             // labelWidth : 150
+//             //width: '450px'
+//             // fieldDefaults: {
+//             //     labelWidth: 200
+//             // }
+//            	//width: '100px'
+//             //allowBlank: false
+//             //maxLength: 100,
+//             //maxWidth: '300px'
+//             //labelWidth: 200
+//         },
+//         {
+//             xtype: 'textfield',
+//             name: 'username',
+//             fieldLabel: 'Username',
 
-            // anchor : '95%',
-            // labelWidth : 150
-            //width: '450px'
-            // fieldDefaults: {
-            //     labelWidth: 200
-            // }
-           	//width: '100px'
-            //allowBlank: false
-            //maxLength: 100,
-            //maxWidth: '300px'
-            //labelWidth: 200
-        }, 
-        {
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
+//             // anchor : '95%',
+//             // labelWidth : 150
+//             //width: '450px'
+//             // fieldDefaults: {
+//             //     labelWidth: 200
+//             // }
+//            	//width: '100px'
+//             //allowBlank: false
+//             //maxLength: 100,
+//             //maxWidth: '300px'
+//             //labelWidth: 200
+//         }, 
+//         {
+//             xtype: 'textfield',
+//             name: 'username',
+//             fieldLabel: 'Username',
 
-            // anchor : '95%',
-            // labelWidth : 150
-            //width: '450px'
-            // fieldDefaults: {
-            //     labelWidth: 200
-            // }
-           	//width: '100px'
-            //allowBlank: false
-            //maxLength: 100,
-            //maxWidth: '300px'
-            //labelWidth: 200
-        }, 
-        {
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
+//             // anchor : '95%',
+//             // labelWidth : 150
+//             //width: '450px'
+//             // fieldDefaults: {
+//             //     labelWidth: 200
+//             // }
+//            	//width: '100px'
+//             //allowBlank: false
+//             //maxLength: 100,
+//             //maxWidth: '300px'
+//             //labelWidth: 200
+//         }, 
+//         {
+//             xtype: 'textfield',
+//             name: 'username',
+//             fieldLabel: 'Username',
 
-            // anchor : '95%',
-            // labelWidth : 150
-            //width: '450px'
-            // fieldDefaults: {
-            //     labelWidth: 200
-            // }
-           	//width: '100px'
-            //allowBlank: false
-            //maxLength: 100,
-            //maxWidth: '300px'
-            //labelWidth: 200
-        }, 
-        {
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
+//             // anchor : '95%',
+//             // labelWidth : 150
+//             //width: '450px'
+//             // fieldDefaults: {
+//             //     labelWidth: 200
+//             // }
+//            	//width: '100px'
+//             //allowBlank: false
+//             //maxLength: 100,
+//             //maxWidth: '300px'
+//             //labelWidth: 200
+//         }, 
+//         {
+//             xtype: 'textfield',
+//             name: 'username',
+//             fieldLabel: 'Username',
 
-            // anchor : '95%',
-            // labelWidth : 150
-            //width: '450px'
-            // fieldDefaults: {
-            //     labelWidth: 200
-            // }
-           	//width: '100px'
-            //allowBlank: false
-            //maxLength: 100,
-            //maxWidth: '300px'
-            //labelWidth: 200
-        }, 
-        {
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
+//             // anchor : '95%',
+//             // labelWidth : 150
+//             //width: '450px'
+//             // fieldDefaults: {
+//             //     labelWidth: 200
+//             // }
+//            	//width: '100px'
+//             //allowBlank: false
+//             //maxLength: 100,
+//             //maxWidth: '300px'
+//             //labelWidth: 200
+//         }, 
+//         {
+//             xtype: 'textfield',
+//             name: 'username',
+//             fieldLabel: 'Username',
 
-            // anchor : '95%',
-            // labelWidth : 150
-            //width: '450px'
-            // fieldDefaults: {
-            //     labelWidth: 200
-            // }
-           	//width: '100px'
-            //allowBlank: false
-            //maxLength: 100,
-            //maxWidth: '300px'
-            //labelWidth: 200
-        },  {
-            xtype: 'textfield',
-            name: 'password',
-            inputType: 'password',
-            fieldLabel: 'Password'
-            //width: 300
-            //allowBlank: false
-         }
-         // {
-        //     xtype: 'displayfield',
-        //     hideEmptyLabel: false,
-        //     value: 'Enter any non-blank password'
-        // }
-        ]      
-});
+//             // anchor : '95%',
+//             // labelWidth : 150
+//             //width: '450px'
+//             // fieldDefaults: {
+//             //     labelWidth: 200
+//             // }
+//            	//width: '100px'
+//             //allowBlank: false
+//             //maxLength: 100,
+//             //maxWidth: '300px'
+//             //labelWidth: 200
+//         },  {
+//             xtype: 'textfield',
+//             name: 'password',
+//             inputType: 'password',
+//             fieldLabel: 'Password'
+//             //width: 300
+//             //allowBlank: false
+//          }
+//          // {
+//         //     xtype: 'displayfield',
+//         //     hideEmptyLabel: false,
+//         //     value: 'Enter any non-blank password'
+//         // }
+//         ]      
+// });
     },
     onAdminPanel : function() {
-    	console.log(this);
+    	//console.log(Ext.Controller);
     }
 
 
